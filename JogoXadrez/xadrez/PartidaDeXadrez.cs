3243ba7a36a1b67c522a45c602a21bb605f80dea
Natalia DAngelo <nataliafdangelo@yahoo.com.br>
2020-03-30 19:22:42 -0300

    Fazendo os primeiros testes de movimentacao

---
+++ JogoXadrez/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using JogoXadrez.tabuleiro;
+
+namespace JogoXadrez.xadrez
+{
+    class PartidaDeXadrez
+    {
+        public Tabuleiro tab { get; private set; }
+        private int turno;
+        private Cor jogadorAtual;
+        public bool terminada { get; private set; }
+
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+            terminada = false;
+            colocarPecas();
+
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retiraPeca(origem);
+            p.incrementarQteMoviemntos();
+            Peca pecaCapturada = tab.retiraPeca(destino);
+            tab.colocarPeca(p, destino);
+
+        }
+
+        private void colocarPecas()
+        {
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c',1).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c',2).toPosicao());
+            tab.colocarPeca(new Rei(tab, Cor.Branca), new PosicaoXadrez('d',1).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('d',2).toPosicao());
+            
+        }
+
+    }
+}
