95f32f8640ffa31d4a069e383e83827b894756e4
Natalia DAngelo <nataliafdangelo@yahoo.com.br>
2020-03-30 17:58:12 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -13,8 +13,10 @@ namespace JogoXadrez
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3,5));
 
                 Tela.imprimirTabuleiro(tab);
             }
